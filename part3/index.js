84841f663ef0eba1664f290f0ca935b3f9e477e7
gosulapranay <gosulapranay@gmail.com>
2024-06-06 11:44:36 +0530

    3.2: Phonebook backend step 2

--- part3/index.js
+++ part3/index.js
@@ -28,5 +28,16 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  let time = new Date();
+  time = time.toString();
+  response.send(
+    `<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${time}</p>    
+    </div>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
