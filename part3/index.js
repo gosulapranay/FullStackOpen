aeffeb893bb8bb482a3cb6603f168c20b53bbf24
gosulapranay <gosulapranay@gmail.com>
2024-06-07 16:33:03 +0530

    3.13: Phonebook database, step 1

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
+const Person = require('./models/person');
 const morgan = require("morgan");
 morgan.token("body", (req) =>
   Object.values(req.body)[0] ? JSON.stringify(req.body) : null
@@ -13,31 +15,31 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => response.json(persons));
 });
 
 app.get("/info", (request, response) => {
@@ -52,9 +54,7 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-  person ? response.json(person) : response.status(404).end();
+  Person.findById(request.params.id).then((persons) => response.json(persons));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -77,5 +77,5 @@ app.post("/api/persons", (request, response) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
