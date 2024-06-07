f0ef2a00f743f909680ee0dfad2654029367a066
gosulapranay <gosulapranay@gmail.com>
2024-06-07 17:27:13 +0530

    3.16: Phonebook database, step 4

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
+
 const express = require("express");
 const app = express();
 app.use(express.json());
+
 const Person = require("./models/person");
+
+const cors = require("cors");
+app.use(cors());
+app.use(express.static("dist"));
+
 const morgan = require("morgan");
 morgan.token("body", (req) =>
   Object.values(req.body)[0] ? JSON.stringify(req.body) : null
@@ -11,35 +18,10 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-const cors = require("cors");
-app.use(cors());
-app.use(express.static("dist"));
-
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
-
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => response.json(persons));
+app.get("/api/persons", (request, response, next) => {
+  Person.find({}).then((persons) =>
+    response.json(persons).catch((error) => next(error))
+  );
 });
 
 app.get("/info", (request, response) => {
@@ -53,8 +35,12 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((persons) => response.json(persons));
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) =>
+      person ? response.json(person) : response.status(404).end()
+    )
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
@@ -68,6 +54,15 @@ app.post("/api/persons", (request, response) => {
   const person = new Person({ name, number });
   person.save().then((savedPerson) => response.json(savedPerson));
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
